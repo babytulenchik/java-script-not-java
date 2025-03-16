82d281cfe994d0b9f05860e3e4476394885e1bac
Eleanor babytulenchik <morskaakapusta633@gmail.com>
2025-03-16 12:47:10 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+//Задание 1: Цикл For
+// for (let i = 0; i<=20; i++) {
+//     if (i % 2 !== 0) continue
+//     alert(i)
+// 
+
+
+//Задание 2: Цикл while
+// let chislo = prompt("Введите число: ")
+// let i = 1
+// while (i <= chislo) {
+//     console.log(i)
+//     i++
+// }
+
+
+// Задание 3: Условное ветвление if
+// let age = prompt("возраст")
+// if (age >= 18){
+//     alert("Вы совершеннолетний")
+// } else {
+//     alert("Вы несовершеннолетний")
+// }
+
+
+// Задание 4: Тернарный оператор
+// let accsessAllowed;
+// let age1 = prompt("возраст")
+// let result = (age1 >= 18) ? true : false
+// console.log(accsessAllowed)
+
+
+// Задание 6: Комбинированное задание
+// let chislo1 = prompt("Введите число: ")
+// if (chislo1 > 0) {
+//     for (let i = 1; i <= chislo1; i++) {
+//         console.log(i)
+//     }
+// } else if (chislo1 < 0) {
+//     alert("Число отрицательное")
+// } else {
+//     alert("Число равно нулю")
+// }
+
+
+//Задание 7: Преобразование строки в число
+// let chislo2 = prompt("Введите число: ")
+// let num = Number(chislo2)
+// if (chislo2 % 2 == 0) {
+//     alert("Число четное");
+// } else {
+//     alert("Число нечетное");
+// }
+
+
+// Задание 8: Сумма чисел с условием
+// let chislo3 = prompt("Введите число: ")
+// if (chislo3 > 10) {
+//     let sum = 0;
+//     for (let i = 1; i <= chislo3; i++) {
+//         sum += i;
+//     }
+//     alert("Сумма всех чисел от 1 до " + chislo3 + " равна: " + sum);
+// } else {
+//     alert("Число слишком маленькое");
+// }
+
+// Задание 9: Поиск делителей
+// let chislo4 = prompt("Введите число: ")
+// for (let i = 1; i <= chislo4; i++) {
+//     if (chislo4 % i === 0) 
+
+// короче я не очень поняла как это сделать я пыталась туда че то добавить но не получилось короче если вы скажете что мы это изучали то я буду в шоке
+// простите p_p
+
+// Задание 10: Факториал числа
+// let chislo5 = prompt("Введите число: ")
+// let factorial = 1;
+// let i = 1;
+// while (i <= chislo5) {
+//     factorial *= i;
+//     i++;
+// }
+// alert("Факториал числа " + chislo5 + " равен: " + factorial);
+
